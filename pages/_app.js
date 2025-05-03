281c8cc83ff12288d17de08c8919634a0f8e18ac
Maceface2 <hmasonostman@gmail.com>
2025-05-02 23:28:00 -0400

    changed website tab name

--- pages/_app.js
+++ pages/_app.js
@@ -15,9 +15,8 @@ export default function App({ Component, pageProps }) {
   return (
     <>
         <Head>
-          <title>MVP Starter</title>
-          <meta name='description' content='Put a description here about your app'/>
-          <meta name='robots' content='index, follow'/>
+          <title>POD</title>
+          <meta name='description' content='Pay On Delivery'/>g
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon_package/apple-touch-icon.png"/>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon_package/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon_package/favicon-16x16.png"/>
